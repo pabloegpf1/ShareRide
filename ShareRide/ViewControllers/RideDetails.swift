00959de2896021818455871277058bf03a03873e
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-02 12:02:14 -0800

    Initial Commit

---
+++ ShareRide/ViewControllers/RideDetails.swift
@@ -0,0 +1,30 @@
+//
+//  RideDetails.swift
+//  ShareRide
+//
+//  Created by Pablo Escriva on 02/03/2019.
+//  Copyright © 2019 SFHacks. All rights reserved.
+//
+
+import UIKit
+
+class RideDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShareRide/ViewControllers/UserAuth.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareRide
+//
+//  Created by Pablo Escriva on 02/03/2019.
+//  Copyright © 2019 SFHacks. All rights reserved.
+//
+
+import UIKit
+
+class UserAuth: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
