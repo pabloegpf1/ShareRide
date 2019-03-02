417da4a2eba56eb8c08f1b3c8f815a7d41354b0e
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-02 12:35:14 -0800

    Added login/signup views

--- ShareRide/ViewControllers/RidesTable.swift
+++ ShareRide/ViewControllers/RidesTable.swift
@@ -7,29 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class RidesTable: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        getRidesList()
     }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 0
+    }
+    
+    func getRidesList() {
+        ref.child("rides").observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let rideID = snap.key
+                print("rideID = \(rideID)")
+            }
+        })
     }
 
     /*
--- ShareRide/ViewControllers/UserAuth.swift
+++ ShareRide/ViewControllers/UserAuth.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  ShareRide
@@ -5,16 +6,91 @@
 //  Created by Pablo Escriva on 02/03/2019.
 //  Copyright © 2019 SFHacks. All rights reserved.
 //
-
 import UIKit
+import FirebaseAuth
 
 class UserAuth: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBOutlet weak var email_login: UITextField!
+    @IBOutlet weak var password_login: UITextField!
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        
+        Auth.auth().signIn(withEmail: email_login.text!, password: password_login.text!) { (user, error) in
+            if error == nil{
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+                
+                
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    
+    @IBOutlet weak var email_signup: UITextField!
+    
+    @IBOutlet weak var password_signup: UITextField!
+    
+    @IBOutlet weak var passwordConfirm_signup: UITextField!
+    
+    
+    @IBAction func signuptriggered(_ sender: UIButton) {
+        
+        if password_signup.text != passwordConfirm_signup.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email_signup.text!, password: password_signup.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func logoutTriggered(_ sender: Any) {
+        
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initial = storyboard.instantiateInitialViewController()
+        UIApplication.shared.keyWindow?.rootViewController = initial
+        
+        
+    }
+    
 }
-
