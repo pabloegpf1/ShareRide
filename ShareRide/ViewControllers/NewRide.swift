5bb55b72a7793672f996655f403a3a7d6ec7a545
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-02 19:20:51 -0800

    New ride

--- ShareRide/ViewControllers/NewRide.swift
+++ ShareRide/ViewControllers/NewRide.swift
@@ -7,24 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
-class NewRide: UIViewController {
-
+class NewRide: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var addressLabel: UITextField!
+    @IBOutlet weak var maxPeopleLabel: UIPickerView!
+    @IBOutlet weak var timeLabel: UIDatePicker!
+    
+    let pickerData: [String] = ["1", "2", "3", "4", "5", "6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.maxPeopleLabel.delegate = self
+        self.maxPeopleLabel.dataSource = self
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func doneButtonClicked(_ sender: Any) {
+        ref.child("rides").childByAutoId()
+            .setValue([
+                "driver": Auth.auth().currentUser?.uid ?? "",
+                "dropoff_location": addressLabel.text!,
+                "start_time": "\(timeLabel.date)",
+                "cost": "3",
+                "max_riders": maxPeopleLabel.selectedRow(inComponent: 0)
+                ])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
 }
